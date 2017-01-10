2a7b61f9a5a9f83b798cc026f5d8820bf1f665d3
Shlygly <Anthony.Champi.Dupont@gmail.com>
2017-01-10 14:06:13 +0100

    Fix Autocomplete
    Fix matchs verification

--- ncscript.lua
+++ ncscript.lua
@@ -111,12 +111,12 @@ hexchat.hook_print('Key Press', function (args)
 					table.insert(matchs, v[1])
 				end
 			end
-			if (table.getn(matchs) > 1) then
+			if (matchs ~= nil and table.getn(matchs) > 1) then
 				hexchat.print(table.concat(matchs, ", "))
 				local mtchstart = getCommonStart(matchs);
 				hexchat.command('SETTEXT !'..mtchstart)
 				hexchat.command('SETCURSOR '..(string.len(mtchstart) + 1))
-			elseif (table.getn(matchs) == 1) then
+			elseif (matchs ~= nil and table.getn(matchs) == 1) then
 				hexchat.command('SETTEXT !'..matchs[1]..' ')
 				hexchat.command('SETCURSOR '..(string.len(matchs[1]) + 2))
 			end
